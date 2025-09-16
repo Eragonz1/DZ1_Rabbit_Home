4340c499b44eb0d0187589023f34d6f1cac56c5d
eragonz <dzolotarev@cchgeu.ru>
2025-09-16 18:37:03 +0300

    Добавьте файлы проекта.

---
+++ DZ1.c
@@ -0,0 +1,24 @@
+﻿#include <locale.h>
+#include <stdio.h>
+
+void main()
+
+{
+	setlocale(LC_ALL, "RUS");
+
+	puts("(\\ (\\     ");
+	puts("");
+	puts("(  -.- )    ");
+	puts("");
+	puts("0__(``)(``) ");
+
+	puts("");
+	puts("");
+	puts("   /\\|| ");
+	puts("  /  \\| ");
+	puts(" /____\\ ");
+	puts("|  _   |");
+	puts("|_| |__|");
+	getchar();
+
+}
